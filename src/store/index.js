32156f7e7a8d3f3de17b23a34bb32890570170e1
= <=>
2025-01-23 16:27:35 +0300

    InvoiceView (Retriewing Invoice)

--- src/store/index.js
+++ src/store/index.js
@@ -8,6 +8,7 @@ export default createStore({
     invoiceModal: null,
     modalActive: null,
     invoicesLoaded: null,
+    currentInvoiceArray: null,
   },
   getters: {},
   mutations: {
@@ -23,6 +24,11 @@ export default createStore({
     },
     INVOICES_LOADED(state) {
       state.invoicesLoaded = true;
+    },
+    SET_CURRENT_INVOICE(state, payload) {
+      state.currentInvoiceArray = state.invoiceData.filter((invoice) => {
+        return invoice.invoiceId === payload;
+      });
     },
   },
   actions: {
